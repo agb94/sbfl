a35763d285ba118eb4fea3bda794d36480106658
Gabin An <agb94@kaist.ac.kr>
2020-02-27 13:57:40 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="agb94", # Replace with your own username
+    name="sbfl",
     version="0.0.1",
     author="Gabin An",
     author_email="agb94@kaist.ac.kr",
